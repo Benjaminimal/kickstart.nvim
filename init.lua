b4b6fd28ef23e399d0caffe5f50270e3b754f638
Benjamin Zinschitz <benjamin@formunauts.at>
2025-01-07 18:49:18 +0100

    enable autopairs

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@
 - review `:Tutor` at some point
 - run AND READ `:help`.
 - make <leader>c aware of splits
-- autopairs
+- redo existing keymaps
 
 --]]
 
@@ -842,7 +842,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
