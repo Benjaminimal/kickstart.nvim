18aa5e1e7ce06b46954b9838ac14a87eb712255d
Benjamin Zinschitz <benjamin@formunauts.at>
2025-01-07 12:38:19 +0100

    add keymaps for split creation

--- init.lua
+++ init.lua
@@ -189,10 +189,12 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left split' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right split' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower split' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper split' })
+vim.keymap.set('n', '<leader>vv', ':<C-u>vsplit<CR>', { desc = 'Create vertical split' })
+vim.keymap.set('n', '<leader>vx', ':<C-u>split<CR>', { desc = 'Create horizontal split' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
